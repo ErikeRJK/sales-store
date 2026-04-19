db3c49d2dac63d77457c4bcf337f55f9ba67277b
ErikeRJK <erike.dev@gmail.com>
2026-04-19 01:10:06 -0300

    fix: baseURL port

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:4505"
+    baseURL: "http://localhost:4507"
 });
 
 instance.interceptors.request.use((config) => {
