b1fd29d7e95951cc35e95c2038aed8c9042c920c
ErikeRJK <erike.dev@gmail.com>
2026-04-19 01:13:07 -0300

    add script clarity in pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        theme="colored"
+      <ToastContainer position="top-right" autoClose={5000} theme="colored" />
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w5nt0e4dp2");
+          `
+        }}
       />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
