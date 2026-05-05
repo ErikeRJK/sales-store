5bca9ee2e212b0ebb1637a7be016a0704ad04104
ErikeRJK <erike.dev@gmail.com>
2026-05-05 18:36:12 -0300

    feature: Implementation next auth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import NextAuth, {DefaultSession} from "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        accessToken?: string;
+        user: {
+            id: string
+        } & DefaultSession["user"]
+    }
+
+    interface User {
+        id: string
+        email: string
+        name: string
+        token: string
+        image?: string 
+
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        accessToken?: strings
+        id?: string
+    }
+}
