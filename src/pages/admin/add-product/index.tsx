03a98379eee5f5a567fcde350dfc7ee74c637e0f
ErikeRJK <erike.dev@gmail.com>
2026-05-14 19:34:18 -0300

    feature: create page admin

---
+++ src/pages/admin/add-product/index.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from "@/components/PageWrapper";
+
+export default function AddProduct(){
+    return(
+        <PageWrapper.Root sidebar={true}>
+            <PageWrapper.Content>
+                <p> Add Product </p>
+            </PageWrapper.Content>
+        </PageWrapper.Root>
+    )
+}
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,10 @@
+import { PageWrapper } from "@/components/PageWrapper";
+
+export default function Admin(){
+    return (
+        <PageWrapper.Root>
+            <PageWrapper.Header/>
+            <p>Admin</p>
+        </PageWrapper.Root>
+    )
+}
